30aa15762ec5c680537848cc9b32787ebcfd8646
Yogendra Rampuria (Yogi) <yrampuria@pivotal.io>
2019-07-16 16:53:09 +0800

    sample

---
+++ samples/cube/index.js
@@ -0,0 +1,5 @@
+module.exports = x => {
+  const cube = x ** 3;
+  console.log(`the cube of ${x} is ${cube}`);
+  return cube;
+};
---
+++ samples/cube/test.js
@@ -0,0 +1,4 @@
+// import { cube } from "./index";
+var cube = require("./index");
+
+console.log(cube(2) === 8);
